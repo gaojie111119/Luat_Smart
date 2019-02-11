464b7c825559a730e76886b06ab16a51bd1b247b
GJ <GJ@LAPTOP-LEH6I6V8>
2019-02-11 13:51:01 +0800

    new

---
+++ script_LuaTask/smart/process.lua
@@ -0,0 +1,16 @@
+--- 模块功能：主程序process
+-- 实现与远程上位机和主控的通信，并监视各传感器状态
+-- @author GJ
+-- @module smart.process
+-- @release 2019.02.11
+
+module(...,package.seeall)
+
+require"pin"
+require"misc"
+require"pm"
+
+
+
+
+
